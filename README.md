29bf20b085d694f41d22c8ad18584477f7e4a39d
felix <sfx86@users.noreply.github.com>
2023-12-05 08:36:47 -0500

    release: 1.0.0

---
+++ README.md
@@ -0,0 +1,13 @@
+# StreetViewer
+
+A simple ArcGIS add-in that opens Google Street View in a browser with a map click.
+
+# Usage
+
+This add-in will place a button in the `Navigate` group on the `Map` toolbar labeled `Open in Street View`. 
+Clicking this tool will bring up a cross with which you can click on your map to open those coordinates in your browser in Google Street View.
+
+# Installation
+
+Grab the latest `.esriAddinX` file from the [release page](https://github.com/sfx86/street-viewer/releases) and put it in a path in which ArcGIS will look for Add-ins.
+The default is `C:\Users\username\Documents\ArcGIS\AddIns\ArcGISPro\`.
---
+++ StreetViewer.cs
@@ -0,0 +1,45 @@
+﻿using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Mapping;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace StreetViewer
+{
+    internal class StreetViewer : MapTool
+    {
+        public StreetViewer()
+        {
+            //
+        }
+        protected override void OnToolMouseDown(MapViewMouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == System.Windows.Input.MouseButton.Left)
+            {
+                e.Handled = true;
+            }
+        }
+        protected override Task HandleMouseDownAsync(MapViewMouseButtonEventArgs e)
+        {
+            return QueuedTask.Run(() =>
+            {
+                var point = ActiveMapView.ClientToMap(e.ClientPoint);
+                var coords = GeometryEngine.Instance.Project(point, SpatialReferences.WGS84) as MapPoint;
+                if (coords == null) { 
+                    return;
+                };
+
+                // q=     : query
+                // layer= : 't' -> traffic, 'c' -> street view, or both
+                // cbll=  : lat, long
+                // cbp=   : map layout: 11 -> lower half map, 12 -> corner map
+                //        : bearing
+                //        : tilt
+                //        : zoom level (0..2)
+                //        : pitch (defaults to 5 deg)
+                var url = $"http://maps.google.com/maps?q=&layer=c&cbll={coords.Y},{coords.X}&cbp=12,0,0,0,0";
+                Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+            });
+        }
+    }
+}
